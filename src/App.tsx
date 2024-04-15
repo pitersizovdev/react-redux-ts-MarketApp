246965592445897bb31a27da413763f9d4cd2cc5
pitersizovdev <pitersizov@gmail.com>
2024-04-15 12:10:43 +0300

    0.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import { MouseEvent, useState } from "react";
 import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Menu from "./pages/Menu/Menu";
+import Cart from "./pages/Cart/Cart";
+import Error from "./pages/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -9,6 +29,15 @@ function App() {
   return (
     <>
       <Button onClick={addCounter}>Button</Button>
+      <Button appearnce="big" onClick={addCounter}>
+        Big Button
+      </Button>
+      <Input placeholder="Email" />
+      <div>
+        <a href="/">Menu</a>
+        <a href="/cart">Cart</a>
+      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,18 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
-const Button = ({ children, className, ...props }: ButtonProps) => {
+const Button = ({
+  children,
+  className,
+  appearnce = "small",
+  ...props
+}: ButtonProps) => {
   return (
     <button
-      className={cn(styles["button"], styles["accent"], className)}
+      className={cn(styles["button"], styles["accent"], className, {
+        [styles["small"]]: appearnce === "small",
+        [styles["big"]]: appearnce === "big",
+      })}
       {...props}
     >
       {children}
